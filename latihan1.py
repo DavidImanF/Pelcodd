7d0e0e2838f5ee99acd4d462569a4407948fdabf
DavidImanF <143859916+DavidImanF@users.noreply.github.com>
2023-09-02 11:00:22 +0700

    Add files via upload
    upload file latihan.

---
+++ latihan1.py
@@ -0,0 +1,31 @@
+nama_saya = str(input("masukkan nama kamu = "))
+print(nama_saya,"bertipe", type(nama_saya))
+
+umur = int(input("masukkan umur kamu = "))
+print(umur,"bertipe", type(umur))
+
+ipk = float(input("masukkan ipk kamu = ") )
+print(ipk, "bertipe", type(ipk))
+
+menikah = bool(input("sudah menikah? = " ))
+print("menikah?", menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("matkul", matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ("web", "pemvis")
+# print("matkul_lain:",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = {"Alpro", "ALpro", "pemweb", "data science", "data science"}
+# print("matkul lain-lain: ", matkul_diambil3, "bertipe", type(matkul_diambil3)) 
+
+# print()
+
+# print(20 / 5)
+# print(50 // 4)
+# print(17 % 3)
+
+# print()
+
+# nama_universitas = "UTM"
+# print(nama_universitas)
